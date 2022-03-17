4aa0e595ac350c30eaeeb76bf0dad93f94cdc6dc
Sohel Rana <sohelcuetcse11@gmail.com>
2022-03-17 22:49:52 +0600

    l

--- movie/src/index.js
+++ movie/src/index.js
@@ -4,5 +4,5 @@ const app = require('./server');
 const PORT = process.env.PORT || 3003;
 
 app.listen(PORT, () => {
-    console.log(`movie svc running at port ${PORT}`);
+    console.log(`movie svc running at ports ${PORT}`);
 });
